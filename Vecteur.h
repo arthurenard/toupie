20d59073e0581420677c66a2ce55365f1eb7b8f9
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-03-03 12:19:16 +0100

    Update Vecteur.h

--- Vecteur.h
+++ Vecteur.h
@@ -1,1 +1,22 @@
-
+#include <iostream> 
+#include <vector>
+#include <math.h> 
+ 
+ class Vecteur {
+	public:
+		void augmente (double);
+		void set_coord (size_t, double);
+		void affiche () const;
+		bool compare (Vecteur const&);
+		Vecteur addition (Vecteur const&) const;
+		Vecteur oppose () const;
+		Vecteur soustraction (Vecteur const&) const;
+		Vecteur mult(double) const;
+		double prod_scal(Vecteur const&) const;
+		Vecteur prod_vect(Vecteur const&) const;
+		double norme () const;
+		double norme2 () const;
+		
+	private:
+		std::vector<double> vecteur;
+};
