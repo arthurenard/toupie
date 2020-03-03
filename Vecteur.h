e8416f38fe1d8c74362e62f693f4bcff30ad0941
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-03-03 16:42:58 +0100

    rajout des operateurs algebriques (P4.3)

--- Vecteur.h
+++ Vecteur.h
@@ -4,30 +4,41 @@
  
  class Vecteur {
 	public:
+	 	//methodes
 		void augmente (double); //rajoute une dimension avec la veleur passé en argument
 		void set_coord (size_t, double); //modifie la n_ieme valeur
-		void affiche (std::ostream&) const; //utiliser "cout" à la place
-		Vecteur addition (Vecteur const&) const; //retourne la somme du vecteur passé en argument avec l'instance
-		Vecteur oppose () const; //retourne le vecteur opposé
-		Vecteur soustraction (Vecteur const&) const; //retourne la difference entre le vecteur passé en argument avec l'instance
-		Vecteur mult(double) const; //multiplie le vecteur par un double
-		double prod_scal(Vecteur const&) const; //retourne le produit scalaire
-		Vecteur prod_vect(Vecteur const&) const; //retourne le produit vectoriel
+		double prod_scal (Vecteur const&) const; //retourne le produit scalaire
 		double norme () const; //retourne la norme euclidienne d'un Vecteur 
 		double norme2 () const; //retourne le carré de la norme euclidienne d'un Vecteur
 		
-		//operateur interne
-		bool operator==(Vecteur const&) const;
-		bool operator!=(Vecteur const&) const;		
-		
-		//constructeur
+		//constructeurs
 		Vecteur (size_t dimension = 0); //construit un Vecteur nulle de telle dimension
 		Vecteur (double, double, double); //construit un Vecteur 3D avec les valeurs passés en argument
 		Vecteur (std::vector<double>); //construit un Vecteur avec la liste passé en argument
+		
+		//operateurs internes
+		bool operator== (Vecteur const&) const;
+		bool operator!= (Vecteur const&) const;		
+		const Vecteur operator- () const; //retourne le vecteur opposé
+		Vecteur& operator+= (Vecteur const&);
+		Vecteur& operator-= (Vecteur const&);		
+		Vecteur& operator*= (double scalaire); //multiplie le Vecteur par un scalaire
+		Vecteur& operator^= (Vecteur const&); 	
+				
+		//inutile au beneficiaire de la classe, privilegier les surcharges d'operateur
+		Vecteur addition (Vecteur const&) const; //retourne la somme du vecteur passé en argument avec l'instance
+		Vecteur soustraction (Vecteur const&) const; //retourne la difference entre le vecteur passé en argument avec l'instance
+		Vecteur mult (double) const; //multiplie le vecteur par un double
+		void affiche (std::ostream&) const; //utiliser "cout" à la place
+		Vecteur prod_vect (Vecteur const&) const; //retourne le produit vectoriel
 		
 	private:
 		std::vector<double> vecteur;
 };
 
 //operateur externe
-std::ostream& operator<<(std::ostream&, Vecteur const&);
+const Vecteur operator+ (Vecteur, Vecteur const&);
+const Vecteur operator- (Vecteur, Vecteur const&);
+const Vecteur operator* (double, Vecteur const&);
+const Vecteur operator^ (Vecteur, Vecteur const&); //retourne le produit vectoriel, dimension 3 uniquement
+std::ostream& operator<< (std::ostream&, Vecteur const&);
