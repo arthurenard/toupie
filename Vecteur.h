b657433ac64fe60ca32f9943c2524ff6b6baf005
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-03-03 15:53:27 +0100

    rajout de commentaires

--- Vecteur.h
+++ Vecteur.h
@@ -4,29 +4,30 @@
  
  class Vecteur {
 	public:
-		void augmente (double);
-		void set_coord (size_t, double);
-		void affiche (std::ostream&) const;
-		Vecteur addition (Vecteur const&) const;
-		Vecteur oppose () const;
-		Vecteur soustraction (Vecteur const&) const;
-		Vecteur mult(double) const;
-		double prod_scal(Vecteur const&) const;
-		Vecteur prod_vect(Vecteur const&) const;
-		double norme () const;
-		double norme2 () const;
+		void augmente (double); //rajoute une dimension avec la veleur passé en argument
+		void set_coord (size_t, double); //modifie la n_ieme valeur
+		void affiche (std::ostream&) const; //utiliser "cout" à la place
+		Vecteur addition (Vecteur const&) const; //retourne la somme du vecteur passé en argument avec l'instance
+		Vecteur oppose () const; //retourne le vecteur opposé
+		Vecteur soustraction (Vecteur const&) const; //retourne la difference entre le vecteur passé en argument avec l'instance
+		Vecteur mult(double) const; //multiplie le vecteur par un double
+		double prod_scal(Vecteur const&) const; //retourne le produit scalaire
+		Vecteur prod_vect(Vecteur const&) const; //retourne le produit vectoriel
+		double norme () const; //retourne la norme euclidienne d'un Vecteur 
+		double norme2 () const; //retourne le carré de la norme euclidienne d'un Vecteur
 		
-		//operateur
+		//operateur interne
 		bool operator==(Vecteur const&) const;
 		bool operator!=(Vecteur const&) const;		
 		
 		//constructeur
-		Vecteur (size_t = 0);
-		Vecteur (double, double, double);
-		Vecteur (std::vector<double>);
+		Vecteur (size_t dimension = 0); //construit un Vecteur nulle de telle dimension
+		Vecteur (double, double, double); //construit un Vecteur 3D avec les valeurs passés en argument
+		Vecteur (std::vector<double>); //construit un Vecteur avec la liste passé en argument
 		
 	private:
 		std::vector<double> vecteur;
 };
 
+//operateur externe
 std::ostream& operator<<(std::ostream&, Vecteur const&);
