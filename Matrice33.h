4e06ad247e67a95d93745fce28f44644f100b754
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-03-05 13:25:08 +0100

    Matrice 3x3 P5, manque le produit avec un autre vecteur

--- Matrice33.h
+++ Matrice33.h
@@ -1,1 +1,39 @@
+#include <iostream> 
+#include <array>
+#include "Vecteur.h"
 
+class Matrice33 {
+	public:
+		//constructeurs
+		Matrice33 (double, double, double); //constructeur de matrice diagonale
+		Matrice33 (double = 0, double = 0, double = 0, double = 0, double = 0, double = 0, double = 0, double = 0, double = 0); //construit la matrice ligne par ligne
+		
+		//methodes
+		Matrice33 transp() const;
+		Matrice33 inv () const;
+		double det() const;
+		
+		//operateurs
+		bool operator== (Matrice33 const&) const;
+		bool operator!= (Matrice33 const&) const;		
+		const Matrice33 operator- () const; //retourne la matrice opposée
+		Matrice33 operator+ (Matrice33 const&) const;
+		Matrice33 operator- (Matrice33 const&) const;
+		Matrice33 operator* (double) const;
+		Matrice33& operator+= (Matrice33 const&);
+		Matrice33& operator-= (Matrice33 const&);		
+		Matrice33& operator*= (double scalaire);
+		//Vecteur operator* (Vecteur const&) const;
+		
+		//methode pour le devellopur
+		void affiche (std::ostream&) const;
+		
+	private:
+		std::array<std::array<double, 3>, 3> matrice;
+		
+		//methodes privees
+};
+
+//operateurs externes
+const Matrice33 operator* (double, Matrice33 const&);
+std::ostream& operator<< (std::ostream&, Matrice33 const&);
