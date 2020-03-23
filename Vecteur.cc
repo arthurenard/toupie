c53c952352b772e41b911d3654679d75ac27fc6b
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-03-23 12:10:29 +0100

    reorganisation du code

--- Vecteur.cc
+++ Vecteur.cc
@@ -1,93 +1,5 @@
 #include "Vecteur.h"
 
-void Vecteur::augmente (double valeur) {
-	vecteur.push_back(valeur);
-}
-
-void Vecteur::set_coord (size_t i, double valeur) {
-	vecteur[i]=valeur;
-}
-
-void Vecteur::affiche (std::ostream &flux) const {
-	std::cout << "(";
-	for (size_t i(0); i < vecteur.size() ; i++) {
-		std::cout << vecteur[i];
-		if (i < vecteur.size()-1) std::cout << ", "; 
-	}
-	std::cout << ")";
-}
-
-Vecteur Vecteur::addition (Vecteur const& autre) const {
-	if (autre.vecteur.size() != vecteur.size()) throw "addition";
-	else {
-		Vecteur retour; 
-		for (size_t i(0) ; i < vecteur.size() ; i++) {
-			retour.augmente(autre.vecteur[i]+vecteur[i]);
-		}
-		return retour;
-	}
-}
-
-Vecteur Vecteur::soustraction (Vecteur const& autre) const {
-	if (autre.vecteur.size() != vecteur.size()) throw "soustraction";
-	else {
-		Vecteur retour; 
-		for (size_t i(0) ; i < vecteur.size() ; i++) {
-			retour.augmente(-autre.vecteur[i]+vecteur[i]);
-		}
-		return retour;
-	}
-}
-
-Vecteur Vecteur::mult (double scal) const {
-	Vecteur retour;
-	for (size_t i(0) ; i < vecteur.size() ; i++) {
-		retour.augmente(scal*vecteur[i]);
-	}
-	return retour;
-}
-
-double Vecteur::prod_scal (Vecteur const& autre) const {
-	if (autre.vecteur.size() != vecteur.size()) throw "produit scal";
-	else {
-		double retour(0); 
-		for (size_t i(0) ; i < vecteur.size() ; i++) {
-			retour+=autre.vecteur[i]*vecteur[i];
-		}
-		return retour;
-	}
-}
-
-Vecteur Vecteur::prod_vect (Vecteur const& autre) const {
-	Vecteur retour; 
-	retour.augmente(vecteur[1]*autre.vecteur[2]-vecteur[2]*autre.vecteur[1]);
-	retour.augmente(vecteur[2]*autre.vecteur[0]-vecteur[0]*autre.vecteur[2]);
-	retour.augmente(vecteur[0]*autre.vecteur[1]-vecteur[1]*autre.vecteur[0]);
-	return retour;
-}
-
-double Vecteur::norme2 () const {
-	double retour(0); 
-	for (size_t i(0) ; i < vecteur.size() ; i++) {
-		retour+=vecteur[i]*vecteur[i];
-	}
-	return retour;
-}
-
-double Vecteur::norme () const {
-	return sqrt(this->norme2());
-}
-
-
-Vecteur Vecteur::unitaire () const {
-	Vecteur retour;
-	double norme = this->norme();
-	for (size_t i(0) ; i < vecteur.size() ; i++) {
-		retour.augmente(norme);
-	}
-	return retour;
-} 
-
 //constructeurs
 
 Vecteur::Vecteur (size_t dim) {
@@ -108,55 +20,44 @@ Vecteur::Vecteur (std::vector<double> liste) {
 	}
 }
 
-//operateurs
+//destructeur
 
-const Vecteur operator+ (Vecteur a, Vecteur const& b){
-	return a.addition(b);
+
+
+//methodes
+
+void Vecteur::augmente (double valeur) {
+	vecteur.push_back(valeur);
 }
 
-const Vecteur operator- (Vecteur a, Vecteur const& b) {
-	return a.soustraction(b);
+void Vecteur::set_coord (size_t i, double valeur) {
+	vecteur[i]=valeur;
 }
 
-const Vecteur operator*(double b, Vecteur const& a) {
-	return a.mult(b);
+double Vecteur::prod_scal (Vecteur const& autre) const {
+	if (autre.vecteur.size() != vecteur.size()) throw "produit scal";
+	else {
+		double retour(0); 
+		for (size_t i(0) ; i < vecteur.size() ; i++) {
+			retour+=autre.vecteur[i]*vecteur[i];
+		}
+		return retour;
+	}
 }
 
-const Vecteur operator*(Vecteur const& a, double b) {
-	return a.mult(b);
+double Vecteur::norme () const {
+	return sqrt(this->norme2());
 }
 
-const Vecteur operator^ (Vecteur a, Vecteur const& b){
-	return a.prod_vect(b);
-}
-
-Vecteur& Vecteur::operator*= (double scalaire) {
-	return *this = (scalaire * *this);
-}
-
-Vecteur& Vecteur::operator-= (Vecteur const& v) {
-	return *this = (*this - v);
-}
-
-Vecteur& Vecteur::operator^= (Vecteur const& v) {
-	return *this = (*this ^ v);
-}
-
-Vecteur& Vecteur::operator+= (Vecteur const& v) {
-	return *this = (*this + v);
-}
-
-const Vecteur Vecteur::operator- () const {
-	Vecteur retour;
+double Vecteur::norme2 () const {
+	double retour(0); 
 	for (size_t i(0) ; i < vecteur.size() ; i++) {
-		retour.augmente(-vecteur[i]);
+		retour+=vecteur[i]*vecteur[i];
 	}
 	return retour;
 }
-	
-const Vecteur Vecteur::operator~ () const {
-	return this->unitaire();
-}
+
+//operateurs internes
 
 bool Vecteur::operator== (Vecteur const& v) const {
 	if (v.vecteur.size() != vecteur.size()) return false;
@@ -168,6 +69,94 @@ bool Vecteur::operator== (Vecteur const& v) const {
 
 bool Vecteur::operator!= (Vecteur const& v) const {
 	return !(*this == v);
+}
+
+const Vecteur Vecteur::operator- () const {
+	Vecteur retour;
+	for (size_t i(0) ; i < vecteur.size() ; i++) {
+		retour.augmente(-vecteur[i]);
+	}
+	return retour;
+}
+	
+const Vecteur Vecteur::operator~ () const {
+	Vecteur retour;
+	double norme = this->norme();
+	for (size_t i(0) ; i < vecteur.size() ; i++) {
+		retour.augmente(norme);
+	}
+	return retour;
+}
+
+const Vecteur Vecteur::operator+ (Vecteur const& autre) const {
+	if (autre.vecteur.size() != vecteur.size()) throw "addition";
+	else {
+		Vecteur retour; 
+		for (size_t i(0) ; i < vecteur.size() ; i++) {
+			retour.augmente(autre.vecteur[i]+vecteur[i]);
+		}
+		return retour;
+	}
+}
+
+const Vecteur Vecteur::operator- (Vecteur const& autre) const {
+	if (autre.vecteur.size() != vecteur.size()) throw "soustraction";
+	else {
+		Vecteur retour; 
+		for (size_t i(0) ; i < vecteur.size() ; i++) {
+			retour.augmente(-autre.vecteur[i]+vecteur[i]);
+		}
+		return retour;
+	}
+}
+
+const Vecteur Vecteur::operator* (double scalaire) const {
+	Vecteur retour;
+	for (size_t i(0) ; i < vecteur.size() ; i++) {
+		retour.augmente(scalaire * vecteur[i]);
+	}
+	return retour;
+}
+
+const Vecteur Vecteur::operator^ (Vecteur const& autre) const { //produit vectoriel, uniquement en dimension 3
+	Vecteur retour; 
+	retour.augmente(vecteur[1]*autre.vecteur[2]-vecteur[2]*autre.vecteur[1]);
+	retour.augmente(vecteur[2]*autre.vecteur[0]-vecteur[0]*autre.vecteur[2]);
+	retour.augmente(vecteur[0]*autre.vecteur[1]-vecteur[1]*autre.vecteur[0]);
+	return retour;
+}
+
+Vecteur& Vecteur::operator+= (Vecteur const& v) {
+	return *this = (*this + v);
+}
+
+Vecteur& Vecteur::operator-= (Vecteur const& v) {
+	return *this = (*this - v);
+}
+
+Vecteur& Vecteur::operator*= (double scalaire) {
+	return *this = (scalaire * *this);
+}
+
+Vecteur& Vecteur::operator^= (Vecteur const& v) {
+	return *this = (*this ^ v);
+}
+
+//inutile aux beneficiaires de la classe
+
+void Vecteur::affiche (std::ostream &flux) const {
+	std::cout << "(";
+	for (size_t i(0); i < vecteur.size() ; i++) {
+		std::cout << vecteur[i];
+		if (i < vecteur.size()-1) std::cout << ", "; 
+	}
+	std::cout << ")";
+}
+
+//methodes externes
+
+const Vecteur operator*(double b, Vecteur const& a) {
+	return a * b;
 }
 
 std::ostream& operator<<(std::ostream& flux, Vecteur const& v) {
