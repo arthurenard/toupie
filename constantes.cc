dd085314d3e0c43f1b38ccf3d02335f750b98ab7
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-04-09 01:18:33 +0200

    rajout de g (double) et pi

--- constantes.cc
+++ constantes.cc
@@ -1,4 +1,6 @@
 #include "constantes.h"
 
-extern const Vecteur g (0.0, 0.0, -9.81); 
+extern const double g (9.81);
+extern const Vecteur vecteur_g (0.0, 0.0, -g); 
 extern const double epsilon (pow(10.0, -9.0));
+extern const double pi (3.14159265358979323846264338327950);
