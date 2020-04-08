4cfd91e546705c631c8c1d8832eb4d5eea5e5360
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-04-09 01:19:57 +0200

    rajout constructeurs et 3 intégrateurs

--- Toupie.h
+++ Toupie.h
@@ -2,25 +2,37 @@
 #define H_TOUPIE
 
 #include <iostream>
+#include <vector>
+#include <cmath> 
+
 #include "Vecteur.h"
+#include "constantes.h"
+
+#define abs(nb) ((nb) < 0 ? -(nb) : (nb))
 
 class Toupie {
 	public: 
 		//constructeurs
-		
+		Toupie (Vecteur vect_P = 0, Vecteur vect_dP = 0);
+		Toupie (std::vector<double> vect_P, std::vector<double> vect_dP);
 		//destructeur
 		
-    //methodes 
-		Vecteur eq_evolution (unsigned long long int temps, Vecteur vect_P, Vecteur vect_dP);
+		//methodes 
+		Vecteur eq_evolution (Vecteur P, Vecteur dP, double temps = 0);
+		
+		void EulerCromer (double pas_de_temps, double temps = 0);
+		void Newmark (double pas_de_temps, double temps = 0);
+		void RungeKutta (double pas_de_temps, double temps = 0);
+		
 		
 		//operateurs internes
 		
 		//inutiles aux beneficiaires 
 		void affiche (std::ostream&) const;
 			
-	private:
-		Vecteur vect_P;
-		Vecteur vect_dP;
+	protected:
+		Vecteur vect_P; // psi, theta, phi
+		Vecteur vect_dP; // psi-point, theta-point, phi-point
 };
 
 //methodes externes
