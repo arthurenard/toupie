af77202360e945226191a38548451f95a19202a9
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-04-28 14:57:47 +0200

    modification après création classe integrable

--- general/toupies/Toupie.h
+++ general/toupies/Toupie.h
@@ -1,47 +1,17 @@
 #ifndef H_TOUPIE
 #define H_TOUPIE
 
-#define CONESIMPLE(1)
-
-
 #include <iostream>
-#include <vector>
-#include <cmath> 
+#include <vector> 
 
 #include "bases/Vecteur.h"
-#include "bases/constantes.h"
+#include "../Integrable.h"
 
-#define abs(nb) ((nb) < 0 ? -(nb) : (nb))
-
-class Toupie {
+class Toupie : public Integrable {
 	public: 
 		//constructeurs
 		Toupie (Vecteur vect_P = 0, Vecteur vect_dP = 0);
 		Toupie (std::vector<double> vect_P, std::vector<double> vect_dP);
-		//destructeur
-		
-		//methodes 
-        virtual Vecteur eq_evolution (Vecteur P, Vecteur dP, double temps = 0)=0;
-		
-		void EulerCromer (double pas_de_temps, double temps = 0);
-		void Newmark (double pas_de_temps, double temps = 0);
-		void RungeKutta (double pas_de_temps, double temps = 0);
-        //accesseurs
-        const Vecteur getP()  {
-            return vect_P;
-        }
-
-		//operateurs internes
-		
-		//inutiles aux beneficiaires 
-		void affiche (std::ostream&) const;
-			
-	protected:
-		Vecteur vect_P; // psi, theta, phi
-		Vecteur vect_dP; // psi-point, theta-point, phi-point
 };
-
-//methodes externes
-std::ostream& operator<< (std::ostream&, Toupie const&);
 
 #endif
