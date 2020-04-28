6bab0f84204e954e52d4486757b2403242c3ea67
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-04-28 14:02:15 +0200

    Création classe intégrable (copie Toupie.h)

---
+++ general/Integrable.h
@@ -0,0 +1,42 @@
+#ifndef H_INTEGRABLE
+#define H_INTEGRABLE
+
+#include <iostream>
+#include <vector>
+#include <cmath> 
+
+#include "bases/Vecteur.h"
+#include "bases/constantes.h"
+
+#define abs(nb) ((nb) < 0 ? -(nb) : (nb))
+
+class Integrable {
+	public: 
+		//constructeurs
+		Toupie (Vecteur vect_P = 0, Vecteur vect_dP = 0);
+		Toupie (std::vector<double> vect_P, std::vector<double> vect_dP);
+		
+    //destructeur
+		
+		//methodes 
+    virtual Vecteur eq_evolution (Vecteur P, Vecteur dP, double temps = 0)=0;
+    const Vecteur get_vect_P const ();
+    
+    void EulerCromer (double pas_de_temps, double temps = 0);
+		void Newmark (double pas_de_temps, double temps = 0);
+		void RungeKutta (double pas_de_temps, double temps = 0);
+    
+		//operateurs internes
+		
+		//inutiles aux beneficiaires 
+		void affiche (std::ostream&) const;
+			
+	protected:
+		Vecteur vect_P; // psi, theta, phi
+		Vecteur vect_dP; // psi-point, theta-point, phi-point
+};
+
+//methodes externes
+std::ostream& operator<< (std::ostream&, Integrable const&);
+
+#endif
