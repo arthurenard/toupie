b7e07fb79f8b79ac05eabceaa67cd256154b3677
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-04-28 15:22:44 +0200

    Rajout de commentaires

--- general/toupies/Toupie.h
+++ general/toupies/Toupie.h
@@ -10,8 +10,8 @@
 class Toupie : public Integrable {
 	public: 
 		//constructeurs
-		Toupie (Vecteur vect_P = 0, Vecteur vect_dP = 0);
-		Toupie (std::vector<double> vect_P, std::vector<double> vect_dP);
+		Toupie (Vecteur vect_P = 0, Vecteur vect_dP = 0); // psi, théta, phi, x, y
+		Toupie (std::vector<double> vect_P, std::vector<double> vect_dP); //psi-point, theta-point, phi-point, x-point, y-point
 };
 
 #endif
