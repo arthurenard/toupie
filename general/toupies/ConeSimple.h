a7456baa234491805ddfaba56340c79287cdd420
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-05-22 20:24:10 +0200

    Suppression de masseVol

--- general/toupies/ConeSimple.h
+++ general/toupies/ConeSimple.h
@@ -8,17 +8,17 @@
 class ConeSimple : public Toupie {
 	public:
 		// constructeurs
-        ConeSimple();
+        	ConeSimple ();
 		ConeSimple (Vecteur vect_P, Vecteur vect_dP, double masseVolumique, double hauteur, double rayon);
 		ConeSimple (std::vector<double> vect_P, std::vector<double> vect_dP, double masseVolumique, double hauteur, double rayon);
 		
 		// destructeurs
 		
 		//methodes publiques
-        virtual Vecteur eq_evolution (Vecteur P, Vecteur dP, double temps = 0) override;
-        //accesseurs
-        double getHauteur(){return hauteur;} // metre
-        double getRayon(){return rayon;}
+        	virtual Vecteur eq_evolution (Vecteur P = 0, Vecteur dP = 0, double temps = 0) override;
+        	//accesseurs
+        	double getHauteur(){return hauteur;} // metre
+        	double getRayon(){return rayon;}
 		//operateurs internes
 
 
@@ -27,18 +27,17 @@ class ConeSimple : public Toupie {
 
 	private: 
 		//attributs
-		double masseVolumique; // kg*m⁻3
 		double hauteur; // metre
 		double rayon; // metre
 		
 		//methodes privees
-        	double volume () const override; // m3
+       		double volume () const override; // m3
         	double I1 () const override; // kg * m2
-       		 double I3 () const override; // kg * m2
-       		 double IA1 () const; // kg * m2
+        	double I3 () const override; // kg * m2
+       		double IA1 () const; // kg * m2
         	double MA () const;
 
-        	Vecteur OG() const override;
+        Vecteur OG() const override;
 };
 
 #endif
