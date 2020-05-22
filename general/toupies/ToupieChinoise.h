a3d54ea3f54b8c383c4a02ca6d7ad6372aef6a2b
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-05-22 20:33:29 +0200

    Suppression de masse volumique

--- general/toupies/ToupieChinoise.h
+++ general/toupies/ToupieChinoise.h
@@ -10,7 +10,7 @@
 class ToupieChinoise : public Toupie {
 	public:
 		// constructeurs
-		ToupieChinoise (Vecteur vect_P, Vecteur vect_dP, double masseVolumique, double rayon, double hauteur);
+        	ToupieChinoise (Vecteur vect_P, Vecteur vect_dP, double masseVolumique, double rayon, double hauteur);
 		ToupieChinoise (std::vector<double> vect_P, std::vector<double> vect_dP, double masseVolumique, double rayon, double hauteur);
 		
 		// destructeurs
@@ -18,24 +18,23 @@ class ToupieChinoise : public Toupie {
 		//methodes publiques
 		
 		//operateurs internes
-        virtual Vecteur eq_evolution (Vecteur P, Vecteur dP, double temps = 0) override;
+        	virtual Vecteur eq_evolution (Vecteur P = 0, Vecteur dP = 0, double temps = 0) override;
 		
 		//methodes inutiles aux beneficiares
 	
 	
 	protected: 
 		//attributs
-		double masseVolumique; // kg*m⁻3
 		double rayon; // metre
 		double hauteur; // metre
 		
 		//methodes privees
-        double volume () const override; // m3
-        double alpha () const;
-        double I1 () const override; // kg * m2
-        double I3 () const override; // kg * m2
+        	double volume () const override; // m3
+        	double alpha () const;
+        	double I1 () const override; // kg * m2
+        	double I3 () const override; // kg * m2
 
-        Vecteur OG () const override; //on considere que G est le centre de la boule pour simplifier les calculs
+        	Vecteur OG () const override; //on considere que G est le centre de la boule pour simplifier les calculs
 };
 
 
