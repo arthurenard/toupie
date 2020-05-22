6203a2aa10c1290c1c764ef68a894a32afe275f2
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-05-22 20:27:59 +0200

    suppression de masse V + correction eq évolution

--- general/toupies/ConeSimple.cc
+++ general/toupies/ConeSimple.cc
@@ -2,23 +2,20 @@
 
 //constructeurs
 ConeSimple::ConeSimple() :
-    Toupie(),
-    masseVolumique(1.0),
-    hauteur(1.0),
-    rayon(1.0)
+    	Toupie(),
+    	hauteur(1.0),
+    	rayon(1.0)
 {}
 
 
 ConeSimple::ConeSimple (Vecteur vect_P, Vecteur vect_dP, double mV, double h, double r) :
-	Toupie (vect_P, vect_dP),
-	masseVolumique (abs(mV)),
+    	Toupie (vect_P, vect_dP, mV),
 	hauteur (abs(h)),
 	rayon (abs(r))
 {}
 
 ConeSimple::ConeSimple (std::vector<double> vect_P, std::vector<double> vect_dP, double mV, double h, double r) : 
-	Toupie (vect_P, vect_dP),
-	masseVolumique (abs(mV)),
+    	Toupie (vect_P, vect_dP, mV),
 	hauteur (abs(h)),
 	rayon (abs(r))
 {}
@@ -28,17 +25,18 @@ ConeSimple::ConeSimple (std::vector<double> vect_P, std::vector<double> vect_dP,
 //methodes publiques
 
 Vecteur ConeSimple::eq_evolution (Vecteur P, Vecteur dP, double temps) {
-    Vecteur retour (5);
-    retour[1] = (1/IA1() ) * (MA() + (vect_dP[0] * sin(vect_P[1]) * ( ( (IA1() - I3()) * vect_dP[0] * cos(vect_P[1])) - (I3() * vect_dP[2]))));
+	Vecteur retour (5);
 
-    if(abs(vect_P[1]) < epsilontheta){
-        retour[0]=0.0;
-        retour[2]=0.0;
-        return retour;
-    }
+    	retour[1] = (1/IA1() ) * (MA() + (dP[0] * sin(P[1]) * ( ( (IA1() - I3()) * dP[0] * cos(P[1])) - (I3() * dP[2]))));
 
-    retour[0] = (vect_dP[1] / (IA1() * sin(vect_P[1]))) * (((I3() - (2 * IA1())) * vect_dP[0] * cos(vect_P[1])) + (I3() * vect_dP[2]));
-    retour[2] = (vect_dP[1] / (IA1() * sin(vect_P[1]))) * ( ( ( vect_dP[0] * ( IA1() - ( ( I3() - IA1() ) * cos(vect_P[1]) * cos(vect_P[1]))) - ( I3() *vect_dP[2] * cos(vect_P[1])))));
+    	if(abs(P[1]) < epsilontheta){
+        	retour[0]=0.0;
+        	retour[2]=0.0;
+        	return retour;
+    	}
+
+    	retour[0] = (P[1] / (IA1() * sin(P[1]))) * (((I3() - (2 * IA1())) * dP[0] * cos(P[1])) + (I3() * dP[2]));
+    	retour[2] = (dP[1] / (IA1() * sin(P[1]))) * ( ( ( dP[0] * ( IA1() - ( ( I3() - IA1() ) * cos(P[1]) * cos(P[1]))) - ( I3() * dP[2] * cos(P[1])))));
 	return retour;
 }
 
@@ -52,15 +50,15 @@ double ConeSimple::volume () const {
 }
 
 double ConeSimple::I1 () const {
-    return (3./20.) * masse() * ((rayon * rayon) + (0.25 * hauteur * hauteur));
+    	return (3./20.) * masse() * ((rayon * rayon) + (0.25 * hauteur * hauteur));
 }
 
 double ConeSimple::I3 () const {
-    return (3./10.) * masse() * rayon * rayon;
+    	return (3./10.) * masse() * rayon * rayon;
 }
 
 double ConeSimple::IA1 () const {
-    return (I1() + ((9./16.) * hauteur * hauteur));
+    	return (I1() + ((9./16.) * hauteur * hauteur));
 }
 
 double ConeSimple::MA () const {
@@ -68,7 +66,7 @@ double ConeSimple::MA () const {
 }
 
 Vecteur ConeSimple::OG() const {
-    return (0.75 * hauteur * OG_unitaire());
+    	return (0.75 * hauteur * OG_unitaire());
 }
 
 //operateurs externes
