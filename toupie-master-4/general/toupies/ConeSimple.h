b5969c7b379fde6f785cb14b6dfde8d71a0a1ed6
arthurenard <46168548+arthurenard@users.noreply.github.com>
2020-05-27 15:02:14 +0200

    Code final (à qlq eq évolu près) \ gestion erreur

---
+++ toupie-master-4/general/toupies/ConeSimple.h
@@ -0,0 +1,30 @@
+#ifndef H_CONESIMPLE
+#define H_CONESIMPLE
+
+#include <iostream> 
+#include "Cone.h"
+#include "bases/constantes.h"
+
+class ConeSimple : public Cone {
+	public:
+		// constructeurs
+        ConeSimple (std::vector<Vecteur>, double masseVolumique, double hauteur, double rayon);
+		
+		// destructeurs
+		
+		//methodes publiques
+        Vecteur eq_evolution (std::vector<Vecteur>, double temps = 0) override;
+
+        int getType() override;
+        	//accesseurs
+
+		//operateurs internes
+
+
+		//methodes inutiles aux beneficiares
+	
+    private:
+        virtual size_t degre () const override;
+};
+
+#endif
